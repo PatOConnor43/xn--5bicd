e2862b42449a9ae27e3499b78bcffcbdfb01245f
Pat O'Connor <patoconnor43@gmail.com>
2022-11-19 20:50:51 -0700

    Add ping route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate lazy_static;
+
+use std::sync::Arc;
+
+use rocket::{
+    http::ext::IntoOwned,
+    request::{FromRequest, Outcome},
+    response::content::RawJson,
+    Request, Rocket,
+};
+
+#[launch]
+async fn rocket() -> _ {
+    // env_logger::init();
+
+    Rocket::build().mount("/", routes!(ping,))
+}
+
+#[get("/ping")]
+fn ping() -> RawJson<&'static str> {
+    RawJson(
+        r#"{
+    "pong": true  
+}"#,
+    )
+}
+
+#[get("/.well-known/webfinger?<resource>")]
+fn finger() -> RawJson<&'static str> {
+    RawJson(r#"{}"#)
 }
