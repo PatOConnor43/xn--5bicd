8eed9d150a88098ca1a6dc65ee70186fa71a494e
Pat O'Connor <patoconnor43@gmail.com>
2022-11-20 21:00:13 -0700

    Implement webfinger for referee account

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,21 @@ extern crate rocket;
 #[macro_use]
 extern crate lazy_static;
 
-use ::webfinger::Webfinger;
+use ::webfinger::{Link, Webfinger};
 use rocket::{http::Status, response::content::RawJson, serde::json::Json};
+
+lazy_static! {
+    static ref DOMAIN: &'static str = "xn--5bicd.fly.dev";
+    static ref ACCOUNT_URL: &'static str = "acct:referee@xn--5bicd.fly.dev";
+    static ref ACTOR_URL: &'static str = "https://xn--5bicd.fly.dev/@referee";
+}
 
 #[launch]
 async fn rocket() -> _ {
     let figment = rocket::Config::figment()
         .merge(("port", 8000))
-        .merge(("address", "0.0.0.0"));
+        .merge(("address", "0.0.0.0"))
+        .merge(("log_level", rocket::config::LogLevel::Debug));
 
     rocket::custom(figment).mount("/", routes![ping, webfinger])
 }
@@ -26,13 +33,24 @@ fn ping() -> RawJson<&'static str> {
 
 #[get("/.well-known/webfinger?<resource>")]
 pub fn webfinger(resource: String) -> Result<Json<Webfinger>, Status> {
-    let webfinger_subject = "acct:@referee@xn--5bicd.fly.dev".to_string();
-    if resource != webfinger_subject {
-        return Err(Status::NotFound);
+    info!("{:?}", resource);
+    let valid_queries = vec![
+        "referee".to_string(),
+        format!("referee@{}", DOMAIN.to_string()),
+        "acct:referee".to_string(),
+        format!("acct:referee@{}", DOMAIN.to_string()),
+    ];
+    if valid_queries.contains(&resource) {
+        return Ok(Json(Webfinger {
+            subject: ACCOUNT_URL.to_string(),
+            aliases: vec![],
+            links: vec![Link {
+                rel: "self".to_string(),
+                href: Some(ACTOR_URL.to_string()),
+                template: None,
+                mime_type: Some("application/ld+json".to_string()),
+            }],
+        }));
     }
-    Ok(Json(Webfinger {
-        subject: webfinger_subject,
-        aliases: vec![],
-        links: vec![],
-    }))
+    Err(Status::NotFound)
 }
